8b17fb13320840870a5df535f2b937e537dc74e1
mario loera <marioll@kth.se>
2024-04-18 14:27:51 -0400

    TestRowToDict working

--- etl_beam/test_etl_beam.py
+++ etl_beam/test_etl_beam.py
@@ -1,3 +1,49 @@
-class TestFoo:
-    def test_foo(self):
-        assert True is True
+import apache_beam as beam
+import fastavro
+from apache_beam.testing.test_pipeline import TestPipeline
+from apache_beam.testing.util import assert_that, equal_to
+from transforms import RowToDict
+
+SCHEMA = {
+    "namespace": "orders.avro",
+    "name": "orders_avro_m2",
+    "type": "record",
+    "fields": [
+        {"name": "name", "type": "string"},
+        {"name": "id", "type": "int"},
+    ],
+}
+
+HEADERS_TYPE = {f["name"]: f["type"] for f in SCHEMA["fields"]}
+PARSED_SCHEMA = fastavro.schema.parse_schema(SCHEMA)
+
+
+class TestRowToDict:
+    INPUT_DATA = [
+        "name;id",
+        "a;1",
+        "b;2",
+    ]
+
+    OUTPUT_DICT = [
+        {"name": "a", "id": 1},
+        {"name": "b", "id": 2},
+    ]
+
+    def test_tuple_str_to_dict(self):
+        # defaul separtor
+        row_to_dict = RowToDict(HEADERS_TYPE)
+        assert {"name": "a", "id": 1} == row_to_dict.tuple_str_to_dict("a;1")
+
+        # ignore headers
+        assert row_to_dict.tuple_str_to_dict("name;id") is None
+
+        # custom separtor
+        row_to_dict = RowToDict(HEADERS_TYPE, separtor="|")
+        assert {"name": "b", "id": 2} == row_to_dict.tuple_str_to_dict("b|2")
+
+    def test_RowToDict(self):
+        with TestPipeline() as pipeline:
+            input_coll = pipeline | beam.Create([self.INPUT_DATA])
+            actual_output = input_coll | beam.ParDo(RowToDict(HEADERS_TYPE))
+            assert_that(actual_output, equal_to(self.OUTPUT_DICT))
