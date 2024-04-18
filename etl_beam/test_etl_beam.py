2ca5cc3107312bded8c5056e6cf92008d54dc05f
mario loera <marioll@kth.se>
2024-04-18 14:34:59 -0400

    TestRowToDict completed

--- etl_beam/test_etl_beam.py
+++ etl_beam/test_etl_beam.py
@@ -2,7 +2,7 @@ import apache_beam as beam
 import fastavro
 from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that, equal_to
-from transforms import RowToDict
+from transforms import RowToDict, dag_row_to_dict
 
 SCHEMA = {
     "namespace": "orders.avro",
@@ -30,6 +30,8 @@ class TestRowToDict:
         {"name": "b", "id": 2},
     ]
 
+    OUTPUT_GRUPED = [(1, OUTPUT_DICT)]
+
     def test_tuple_str_to_dict(self):
         # defaul separtor
         row_to_dict = RowToDict(HEADERS_TYPE)
@@ -47,3 +49,9 @@ class TestRowToDict:
             input_coll = pipeline | beam.Create([self.INPUT_DATA])
             actual_output = input_coll | beam.ParDo(RowToDict(HEADERS_TYPE))
             assert_that(actual_output, equal_to(self.OUTPUT_DICT))
+
+    def test_dag_row_to_dict(self):
+        with TestPipeline() as pipeline:
+            input_coll = pipeline | beam.Create([self.INPUT_DATA])
+            actual_output = dag_row_to_dict(input_coll, HEADERS_TYPE)
+            assert_that(actual_output, equal_to(self.OUTPUT_GRUPED))
